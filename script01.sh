d39ad606b6d64e5df12261135b20bec60bc003d2
Yasmim Tonezi <ystonezi@fitec.org.br>
2025-06-26 13:24:31 -0300

    Add Scripts

---
+++ script01.sh
@@ -0,0 +1,7 @@
+echo "ABC" > teste01.txt
+mv teste01 teste02.txt
+chmod 777 teste02.txt
+cat teste02.txt
+cat teste02.txt > teste03.txt
+ls -l
+
---
+++ script02.sh
@@ -0,0 +1,11 @@
+sudo vi texto
+mkdir diretorio 01
+suco cp texto diretorio01
+ls diretorio01
+cat texto
+cd diretorio01
+mkdir pastateste
+ls -l
+rmdir pastateste
+pwd
+cd ..
